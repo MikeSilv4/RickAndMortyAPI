3b81fa1c1ed247a467c6acff326401d84e162a72
Mikesilva <mike.oli.silva1@gmail.com>
2023-10-10 13:38:25 -0300

    adicionando o css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let pagina = 1;
     // Retorna a tela principal
 function telaPrincipal () {
 
-    resetaCampoDigitacao('Digite ID/NOME');
+    resetaCampoDigitacao('Digite ID/NOME(Apos escolher a opreção)');
 
     const codMain = `
         <div class="corpoPersonagens"></div>
